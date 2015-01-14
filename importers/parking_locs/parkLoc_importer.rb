1fee712342807a35d92e8b0f59a29bbf208043f8
Tom Demeyer <tom@waag.org>
2015-01-14 17:37:47 +0100

    parking garages importer

---
+++ importers/parking_locs/parkLoc_importer.rb
@@ -0,0 +1,73 @@
+#encoding: utf-8
+
+require 'json'
+require 'citysdk'
+include CitySDK
+
+# ============================ Get endpoint and owner credentials ==============================
+
+config_path = nil
+if ARGV.length > 0
+  config_path = ARGV[0]
+elsif ENV.has_key? 'CITYSDK_CONFIG' and ENV['CITYSDK_CONFIG'].length
+  config_path = ENV['CITYSDK_CONFIG']
+end
+
+config = nil
+begin
+  config = JSON.parse(File.read(config_path), {symbolize_names: true})
+rescue Exception => e
+  $stderr.puts <<-ERROR
+  Error loading CitySDK configuration file...
+  Please set CITYSDK_CONFIG environment variable, or pass configuration file as a command line parameter
+  Error message: #{e.message}
+  ERROR
+  exit!(-1)
+end
+
+# ================================== Create 'admr' layer ==================================
+
+$park_layer = JSON.parse(File.read("#{File.dirname(__FILE__)}/parkLoc_layer.json"), symbolize_names: true)
+
+# Set JSON-LD context
+$park_layer[:context] = {
+  :"@vocab" => "#{config[:endpoint][:url]}layers/parking.garage/fields/"
+}
+
+$api = API.new(config[:endpoint][:url])
+if not $api.authenticate(config[:owner][:name], config[:owner][:password])
+  puts 'Error authenticating with API'
+  exit!(-1)
+end
+
+$stderr.puts ("Deleting layer 'parking.garage' and objects, if layer already exists...")
+begin
+  $api.delete('/layers/parking.garage')
+rescue CitySDK::HostException => e
+  
+end
+$stderr.puts ("Creating layer 'parking.garage'")
+$api.post("/layers", $park_layer)
+
+$api.batch_size = 25
+
+# ==================================== Municipalities ====================================
+
+$stderr.puts ('Importing parking locations')
+params = {
+  file_path: './data/ParkingLocation.json',
+  title: :Name,
+  host: config[:endpoint][:url],
+  layer: 'parking.garage',
+  name: config[:owner][:name],
+  password: config[:owner][:password]
+}
+
+imp = Importer.new(params)
+
+imp.do_import do |object_datum|
+  
+  puts JSON.pretty_generate(object_datum)
+
+end
+
